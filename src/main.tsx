b6495127d7e3679638c778b7dc9d1632e4383281
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-05-11 12:46:14 +0200

    initial cleaning of files

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import App from './App.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <h1 className="text-4xl font-bold text-center text-indigo-600 mt-8 mb-4 hover:text-blue-500">
-      Hello TW
-    </h1>
+  
   </StrictMode>
 );
