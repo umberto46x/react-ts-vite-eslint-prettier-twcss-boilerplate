aa4a0f233d151f0137fe1182b4ac2a21e710725f
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-05-11 13:01:40 +0200

    installed testing library

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   testEnvironment: "node",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
   },
