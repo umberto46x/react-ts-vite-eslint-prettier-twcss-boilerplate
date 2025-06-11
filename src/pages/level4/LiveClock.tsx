caffff629b375493b4e04d2699358716d38fa74f
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-06-11 18:16:25 +0200

    fix: rimuove la dipendenza da 'time' nell'effetto per evitare aggiornamenti non necessari

--- src/pages/level4/LiveClock.tsx
+++ src/pages/level4/LiveClock.tsx
@@ -11,13 +11,13 @@ export const LiveClock = () => {
     return () => {
       clearInterval(interval);
     };
-  }, [time]);
+  }, []);
   return (
     <>
       <div className="w-75 mx-auto bg-gray-500 m-4 text-center pb-10    ">
         <h4 className="text-center p-4 text-bold text-2xl">Live Clock</h4>
-        <p className="text-5xl text-center  mt-5 mb-10">Time </p>
-        <span className="text-5xl text-center border-5 rounded-xl p-1    ">
+        <p className="text-5xl text-center  mt-5 mb-10 ">Time </p>
+        <span className="text-5xl text-center border-5 rounded-xl p-1 animate-pulse    ">
           {time}{' '}
         </span>
       </div>
